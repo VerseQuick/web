5f48ae260c7550f5bed29ce1e6921f80822239a6
Berin Aniesh <berinaniesh@gmail.com>
2024-07-28 12:57:10 +0530

    deployed in new url

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 npm i
 npm run build
-rm bible-web.zip 2> /dev/null
-zip -r bible-web.zip .
-scp bible-web.zip berinaniesh.xyz:/home/berinaniesh/tmp/
-ssh berinaniesh.xyz deploy-bible-web.sh
+rm versequick-web.zip 2> /dev/null
+zip -r versequick-web.zip .
+scp versequick-web.zip versequick.com:/home/berinaniesh/tmp/
+ssh versequick.com deploy-versequick-web.sh
