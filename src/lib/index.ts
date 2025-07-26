d5d2f48665d9283cd40742a0507217c005d72b95
Berin Aniesh <berinaniesh@gmail.com>
2025-07-26 06:45:25 +0530

    add options to copy as paragraph and include link to site while copying

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 export const API_URL: string = 'https://api.versequick.com';
+export const SITE_URL: string = 'https://versequick.com';
